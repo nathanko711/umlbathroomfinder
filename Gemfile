600202b417ce5507f96ce3449ecb8107753f9e5a
nate ko <nathanko711@gmail.com>
2017-04-13 19:42:21 +0000

    Users working and administrator working

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'jbuilder',     '2.4.1'
 gem 'bcrypt',       '3.1.11'
 gem 'jquery-turbolinks'
 gem 'bootstrap-sass', '3.3.6'
+gem 'faker',          '1.6.6'
+gem 'will_paginate',           '3.1.0'
+gem 'bootstrap-will_paginate', '0.0.10'
 
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def show
     @user = User.find(params[:id])
   end
@@ -33,6 +40,12 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
   private
 
@@ -58,5 +71,10 @@ class UsersController < ApplicationController
         redirect_to(root_url)
       end
       # redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
