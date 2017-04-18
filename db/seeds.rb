93a5e3370f58967e86710b76b4c530d6ea4ec2bd
nate ko <nathanko711@gmail.com>
2017-04-18 01:21:56 +0000

    Changes the Admin Password for the Beta Demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Admin User",
+             email: "admin@umlbathroomfinder.com",
+             password:              "adminpwd",
+             password_confirmation: "adminpwd",
                           admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "example-#{n+1}@umlbathroomfinder.com"
   password = "password"
   User.create!(name:  name,
                email: email,
