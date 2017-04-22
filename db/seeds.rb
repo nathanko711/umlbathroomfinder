f47739745ccf3947b7db554a78a23eeafec19db3
nate ko <nathanko711@gmail.com>
2017-04-22 01:51:41 +0000

    Adds 4 admins to db seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,23 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "nko",
+             email: "nathan_ko@student.uml.edu",
+             password:              "adminpwd",
+             password_confirmation: "adminpwd",
+                          admin: true)
+
+User.create!(name:  "jrothmel",
+             email: "jacob_rothmel@student.uml.edu",
+             password:              "adminpwd",
+             password_confirmation: "adminpwd",
+                          admin: true)
+                          
+User.create!(name:  "cvu",
+             email: "chuong_vu@student.uml.edu",
+             password:              "adminpwd",
+             password_confirmation: "adminpwd",
+                          admin: true)
+                          
+User.create!(name:  "smello",
+             email: "scott_mello@student.uml.edu",
+             password:              "adminpwd",
+             password_confirmation: "adminpwd",
                           admin: true)
